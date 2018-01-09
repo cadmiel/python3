d4cce08e922d617ce81212a1ca0077197f832105
GABRIL\T41533 <codmiel.chitende_MAZZATECH@abril.com.br>
2018-01-09 18:04:39 -0200

    add google e wikipedia

--- Google.py
+++ Google.py
@@ -1,7 +1,27 @@
 from google import search
+import wikipedia
 
 searchTerm = input('Procurar por: ')
 
-print('Resultado d a pesquisa:')
-for url in search(searchTerm, stop=10):
-    print(url)
+opcao = True
+while opcao:
+    contador = 0
+    print('Onde deseja pesquisar? ')
+    print('1 - Google')
+    print('2 - Wikipedia')
+    print('3 - Sair')
+    escolha = int(input('> '))
+    print('Por favor aguarde pesquisando...')
+    if escolha == 1:
+        for url in search(searchTerm, stop=10):
+            print(url)
+    elif escolha == 2:
+        wikipedia.set_lang("pt")
+        for results in  wikipedia.search(searchTerm):
+            print(wikipedia.summary(results, sentences=1))
+    elif escolha == 3:
+        opcao = False
+        break
+    else:
+        print('Opção invalida, por favor selecione outra opcao')
+        opcao = True
